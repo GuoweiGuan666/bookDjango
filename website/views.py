358ec00635730ba4f43d0ef33e19047a4ba6f8ed
Guowei Guan <guoweiguan2016@gmail.com>
2021-12-01 08:23:55 -0800

    inital commit

---
+++ website/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.core.mail import send_mail
+
+def home(request):
+	return render(request, 'home.html', {})
+
+def contact(request):
+	if request.method == "POST":
+		#do stuff
+		message_name = request.POST['message-name']
+		message_email = request.POST['message-email']
+		message = request.POST['message']
+
+		#send email function
+		send_mail(
+			message_name, #subject or title of the email
+			message, #the message itself
+			message_email, #the email filled in the form and send us the message
+			['guoweiguan2016@gmail.com'], #our email address that receive this notice
+			fail_silently = False,
+			)
+
+		return render(request, 'contact.html', {'message_name': message_name, 'message_email': message_email})
+
+	else:
+		#return the page
+		return render(request, 'contact.html', {})
+
+
+
+
